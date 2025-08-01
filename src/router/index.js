db524a8db38e8fce888d0819e75ad0bc1756bfa0
你的GitHub用户名 <GitHub>
2025-08-01 16:07:09 +0800

    修改滑动到顶部

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    // 如果有保存的位置（浏览器前进后退），则恢复到该位置
+    if (savedPosition) {
+      return savedPosition
+    }
+    // 否则滚动到顶部
+    return { top: 0 }
+  },
   routes: [
     {
       path: '/',
